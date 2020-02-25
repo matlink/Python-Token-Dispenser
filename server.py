ed5137f11b69187b69a509779abf9affd445b6a7
Matlink <matlink@matlink.fr>
2020-02-25 18:31:33 +0100

    Support device_codename

--- server.py
+++ server.py
@@ -9,8 +9,15 @@ class MainHandler(tornado.web.RequestHandler):
 	def initialize(self, credentials_list):
 		self.credentials_list = credentials_list
 
-	def get(self):
-		api = GooglePlayAPI()
+	def get(self, device=None):
+		if device is None:
+			api = GooglePlayAPI()
+		else:
+			try:
+				api = GooglePlayAPI(device_codename=device)
+			except Exception as e:
+				self.write("Error: no such device %s" % device)
+				return
 		email, password = random.choice(self.credentials_list).split()
 		api.login(email, password)
 		self.write("%s %s" % (api.authSubToken, hex(api.gsfId)[2:]))
@@ -19,7 +26,8 @@ def make_app(credential_file):
 	with open(credential_file) as inbuff:
 		credentials_list = list(inbuff)
 	return tornado.web.Application([
-		(r"/", MainHandler, dict(credentials_list = credentials_list)),
+		(r"/email/gsfid/(.*)", MainHandler, dict(credentials_list = credentials_list)),
+		(r"/.*", MainHandler, dict(credentials_list = credentials_list)),
 	])
 
 if __name__ == "__main__":
